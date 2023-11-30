2d69ad32d4f43f4755e333548e5e3f4a744a1066
daffailhamramadan <daffailhamramadan127@gmail.com>
2023-11-30 19:27:13 +0700

    Add google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,15 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
+import partytown from '@astrojs/partytown'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astrofy-template.netlify.app',
-  integrations: [mdx(), sitemap(), tailwind()]
+  site: 'https://daffailhamramadan.github.io',
+  integrations: [mdx(), sitemap(), tailwind(), partytown({
+    config: {
+      forward: ["dataLayer.push"],
+    },
+  })
+  ]
 });
